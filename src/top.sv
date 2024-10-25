95232b664e276d6a6fb20096684f8b3e24fd8208
ushiroda <ushiroda@nanja.info>
2024-10-25 21:22:41 +0900

    dalhi_oe_n = !dalhi_oe_n

--- src/top.sv
+++ src/top.sv
@@ -16,17 +16,17 @@ module top (
     input wire [3:0] a,         // A<3:0>
     input wire bufctl_n,
     input wire ale_n,
-    output wire nxm_n,
+    output wire nxm,
     input wire sctl_n,
     input wire clk,
     output reg dv,
     output reg miss_n,
     output reg dallo_oe_n,
-    output reg dalhi_oe_n,
-    input wire rrdy,            // Ready to read from DCJ11's ODT data
-    output reg rstb,            // Strobe for read from DCJ11's ODT data
-    input wire wrdy,            // Ready to write to DCJ11's ODT data
-    output reg wstb,            // Strobe for write to DCJ11's ODT data
+    output reg event_n,
+    input wire rrdy_n,            // Ready to read from DCJ11's ODT data
+    output reg rstb_n,            // Strobe for read from DCJ11's ODT data
+    input wire wrdy_n,            // Ready to write to DCJ11's ODT data
+    output reg wstb_n,            // Strobe for write to DCJ11's ODT data
     inout wire [7:0] ad,        // ODT data
     output wire [1:0] O_psram_ck,
     output wire [1:0] O_psram_ck_n,
@@ -103,7 +103,6 @@ always_ff@(posedge clk_x3) begin
     end else begin
         if (count == 0) begin
             dallo_oe_n <= 1'b1;
-            dalhi_oe_n <= 1'b0;
             mdallo <= dal;
         end else if (count == 1) begin
             if ((aio == GP_READ) || (aio == GP_WRITE)) begin
@@ -122,66 +121,65 @@ always_ff@(posedge clk_x3) begin
             mdal[16] <= dal[12];
             mdal[15:0] <= mdallo;
             dallo_oe_n <= 1'b0;
-            dalhi_oe_n <= 1'b1;
         end
         count <= count + 1'b1;
     end
 end
 
-assign nxm_n = sctl_n ? 1'b1 :
-    ((maio[2] == 0) && (mbs == BS_MEM) && (mdal > HIMEM)) ? 1'b0 :
-    ((maio[2] == 0) && (mbs == BS_EXT) && ((mdal[21:3] != DLART) && (mdal[21:3] != PC11))) ? 1'b0 :
-    1'b1;
+assign nxm = sctl_n ? 1'b0 :
+    ((maio[2] == 0) && (mbs == BS_MEM) && (mdal > HIMEM)) ? 1'b1 :
+    ((maio[2] == 0) && (mbs == BS_EXT) && ((mdal[21:3] != DLART) && (mdal[21:3] != PC11))) ? 1'b1 :
+    1'b0;
 
 logic rxrdy;
 logic rrdy1;
 logic rrdy0;
 logic [7:0] rdata;
 always_ff@(negedge clk) begin
-    rrdy0 <= rrdy;
+    rrdy0 <= !rrdy_n;
     rrdy1 <= rrdy0;
 end
 
 always_ff@(negedge clk) begin
     if ((sctl_n == 0) && (mdal == XBUF)) begin
-        rstb <= 1'b1;
+        rstb_n <= 1'b0;
         rxrdy <= 1'b0;
         rdata <= dal[7:0];
     end else if (!rrdy1) begin
-        rstb <= 1'b0;
-    end else if (!rstb) begin
+        rstb_n <= 1'b1;
+    end else if (rstb_n) begin
         rxrdy <= 1'b1;
     end
     if (gp_code == 8'o014) begin
         rxrdy <= 1'b1;
-        rstb <= 1'b0;
+        rstb_n <= 1'b1;
     end
 end
 
-assign ad = rstb ? rdata : 8'bz;
+assign ad = rstb_n ? 8'bz : rdata;
 
 logic wxrdy;
 logic wrdy1;
 logic wrdy0;
 logic [7:0] wdata;
 always_ff@(negedge clk) begin
-    wrdy0 <= wrdy;
+    wrdy0 <= !wrdy_n;
     wrdy1 <= wrdy0;
 end
 
 always_ff@(negedge clk) begin
     if (wrdy1) begin
-        wstb <= 1'b1;
-    end else if (wstb) begin
+        wstb_n <= 1'b0;
+    end else if (!wstb_n) begin
         wdata <= ad;
-        wstb <= 1'b0;
+        wstb_n <= 1'b1;
         wxrdy <= 1'b1;
     end else if ((mdal == RBUF) && (!sctl_n)) begin
         wxrdy <= 1'b0;
     end
     if (gp_code == 8'o014) begin
         wxrdy <= 1'b0;
-        wstb <= 1'b0;
+        wstb_n <= 1'b1;
     end
 end
 
